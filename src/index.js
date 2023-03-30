a9a680eef5105e5e4a2075d50ef0d9edd9ad41e2
zal <3163152549@qq.com>
2023-03-30 11:10:00 +0800

    procider组件的使用

--- src/index.js
+++ src/index.js
@@ -7,16 +7,21 @@ import { BrowserRouter } from "react-router-dom"
 // // 引入App组件
 import App from "./App"
 import store from "./redux/store"
+import { Provider } from 'react-redux'
 import "./App.css"
 
 const root = createRoot(document.getElementById('root'))
-root.render(<BrowserRouter><App /></BrowserRouter>)
+root.render(
+    <Provider store={store}>
+        <BrowserRouter><App /></BrowserRouter>
+    </Provider>
+)
 
 // 监测redux中状态的改变， 如redux的状态发生了改变， 那么冲洗渲染App早已组件
-store.subscribe(() => {
-    // // 渲染到App页面
-    root.render(<BrowserRouter><App /></BrowserRouter>)
-})
+// store.subscribe(() => {
+//     // // 渲染到App页面
+//     root.render(<BrowserRouter><App /></BrowserRouter>)
+// })
 
 
 // const root = createRoot(document.getElementById('root'))
